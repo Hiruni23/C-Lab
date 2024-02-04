511cc63f3946964ef0185b53be460a1519b215a3
HIruni  Ganege <132427038+Hiruni23@users.noreply.github.com>
2024-02-04 13:04:46 +0530

    Add files via upload
    C#Lab

---
+++ LabSheet1(Day02)/Q4Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet1Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of N: ");
+            int num = int.Parse(Console.ReadLine());
+
+           
+            for (int i = 0; i <= num; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+
+            Console.ReadLine(); 
+        }
+
+        static int Fibonacci(int i)
+        {
+            if (i <= 1)
+                return i;
+            else
+                return Fibonacci(i - 1) + Fibonacci(i - 2);
+        }
+    }
+}
+    
+
