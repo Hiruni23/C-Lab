b114f8d92077b6156489348533fde92ac9a1ae3c
HIruni  Ganege <132427038+Hiruni23@users.noreply.github.com>
2024-02-04 13:14:21 +0530

    Add files via upload
    C#Lab

---
+++ LabSheet1(Day01)/Q1Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Labsheet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter the length");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the width");
+            double width = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("The area of the Rectangle is = "+ length * width);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
